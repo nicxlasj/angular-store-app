cd5f1cb3ef8d97fb7db07ef9406ef90d607e295f
nico <nicolasjaiquel@gmail.com>
2024-02-22 14:09:49 -0500

    Añadiendo pagina de about

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,22 @@
 import { Routes } from '@angular/router';
 import { ListComponent } from './domains/products/pages/list/list.component';
 import { NotFoundComponent } from './domains/shared/not-found/not-found.component';
+import { LayoutComponent } from './domains/shared/layout/layout.component';
+import { AboutComponent } from './domains/info/about/about.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: ListComponent,
+    component: LayoutComponent,
+    children: [{
+        path: '',
+        component: ListComponent
+      },
+      {
+        path: 'about',
+        component: AboutComponent
+      }
+      ]
   },
   {
     path: '**',
--- src/app/domains/shared/header/header.component.ts
+++ src/app/domains/shared/header/header.component.ts
@@ -3,11 +3,12 @@ import { Product } from '../models/product.model';
 import { ProductCartComponent } from '../../products/components/product-cart/product-cart.component';
 import { CartService } from '../services/cart.service';
 import { CurrencyPipe } from '@angular/common';
+import { RouterLinkActive, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ProductCartComponent, CurrencyPipe],
+  imports: [ProductCartComponent, CurrencyPipe, RouterModule, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
